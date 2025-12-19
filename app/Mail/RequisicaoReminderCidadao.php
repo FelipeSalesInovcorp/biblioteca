46c73e9a976f0def887c70fb1305e2fd99593c45
FelipeSalesInovcorp <felipe.sales@trainee.inovcorp.com>
2025-12-19 01:31:41 +0000

    feat: lembretes por email para requisições

---
+++ app/Mail/RequisicaoReminderCidadao.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Requisicao;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+
+class RequisicaoReminderCidadao extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(public Requisicao $requisicao) {}
+
+    public function build()
+    {
+        $livro = $this->requisicao->livro;
+
+        $mail = $this->subject('Lembrete: entrega amanhã da Requisição #'.$this->requisicao->numero_sequencial)
+            ->markdown('emails.requisicoes.reminder_cidadao', [
+                'requisicao' => $this->requisicao,
+                'livro' => $livro,
+                'cidadao' => $this->requisicao->user,
+            ]);
+
+        if ($livro?->imagem_capa && Storage::disk('public')->exists($livro->imagem_capa)) {
+            $mail->attach(
+                Storage::disk('public')->path($livro->imagem_capa),
+                [
+                    'as' => 'capa-livro-'.$livro->id.'.png',
+                    'mime' => 'image/png',
+                ]
+            );
+        }
+
+        return $mail;
+    }
+}
