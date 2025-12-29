d8a78184b4bc9977ed2590eb92ca10a64e1b83a1
FelipeSalesInovcorp <felipe.sales@trainee.inovcorp.com>
2025-12-29 11:06:14 +0000

    Ajustes finais na integração com o Google Books

--- resources/views/admin/googlebooks/index.blade.php
+++ resources/views/admin/googlebooks/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl leading-tight text-center text-base-content leading-tight text-blue-800">Google Books — Importação</h2>
+        <h2 class="font-semibold text-xl leading-tight text-center text-base-content leading-tight text-blue-800">📥 Google Books — Importação</h2>
     </x-slot>
 
     <div class="p-6 space-y-6">
